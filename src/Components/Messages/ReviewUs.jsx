ae9e14fdb409bda4736cf554667a3139c0202b23
Gurudas Bhardwaj <gursad5@gmail.com>
2025-10-10 11:29:41 +0530

    Update

--- src/Components/Messages/ReviewUs.jsx
+++ src/Components/Messages/ReviewUs.jsx
@@ -29,7 +29,7 @@ export default function ReviewPage() {
     console.log(rating)
 
     try {
-      const response = await fetch("http://localhost:9000/other/giveReview", {
+      const response = await fetch("https://ai-based-quiz-builder-quizfy-backend.onrender.com/other/giveReview", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
